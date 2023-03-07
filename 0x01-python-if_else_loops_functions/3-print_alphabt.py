55d6e63791efcf983aaa9e601bad0e80b8f89f21
zfiromsa <mdzm941@gmail.com>
2023-03-07 11:16:59 -0800

    go

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter_code in range(ord('a'), ord('z')+1):
-    letter = chr(letter_code)
-    if letter not in "qe":
-        print(letter, end="")
+for alpha_letters in range(ord('a'), ord('z')+1):
+    if alpha_letters == 113 or alpha_letters == 101:
+       continue
+    print("{:c}".format(alpha_letters), end="")
