c6238a4412aba69b870a0782f960080b0d720fc4
zfiromsa <mdzm841@gmail.com>
2023-03-08 15:01:28 -0800

    go

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
+_end = " , "
+for i in range(0, 90):
+    r = i % 10
+    s = i / 10
+    if i < 0:
+        print("{}{}".format(0,i), end=_end)
+    else:
+        if i == 89:
+            _end = "\n"
+        print(i, end=_end)
 
