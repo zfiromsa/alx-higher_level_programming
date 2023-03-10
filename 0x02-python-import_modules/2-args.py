6ee1c87992d27cf1dc2d816125446585d902d34e
zfiromsa <mdzm941@gmail.com>
2023-03-09 23:24:35 -0800

    go1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    n = len(sys.argv)
-    if n == 1:
-        print("0 arguments.")
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("{} arguments.".format(n))
     else:
         print(n, "arguments:")
         for i in range(1, n):
