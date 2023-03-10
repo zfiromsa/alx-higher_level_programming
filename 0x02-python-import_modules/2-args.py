3a145a32f94be62061a3a8404d45edbc248e9a9d
zfiromsa <mdzm941@gmail.com>
2023-03-09 23:01:32 -0800

    go

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,6 +2,9 @@
 import sys
 if __name__ == "__main__":
     n = len(sys.argv)
-    for i in range(1, n):
-        pr = "{}: {}".format(i, sys.argv[i])
-        print(pr)
+    if n == 1:
+        print("")
+    else:
+        print(n, "argument:")
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))
