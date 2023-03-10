c241a2de55c927d1d41634c36a11f4172342d333
zfiromsa <mdzm841@gmail.com>
2023-03-09 22:10:14 -0800

    go

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#!/bin/python3
 import sys
 if __name__ == "__main__":
-    count = len.argv
+    count = len(sys.argv)
     for i in range(1, count):
-        print(argv[count])
+        print(sys.argv[count])
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    count = 0
+    _sum = 0
     for i in sys.argv:
-        count += i
-    print(count)
+        _sum = _sum + i
+    print(_sum)
