fb4152781a729ae4435883a4a2c392a4dde1c950
zfiromsa <mdzm941@gmail.com>
2023-03-13 02:18:20 -0700

    go

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    counter = 0
+    for item in my_list:
+        counter += 1
+    for i in range(0, counter):
+        print("{}".format(my_list[i]))
