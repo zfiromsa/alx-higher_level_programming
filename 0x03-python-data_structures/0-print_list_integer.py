65b019f2bfc918ee39f9008cecf65f8041a633be
zfiromsa <mdzm941@gmail.com>
2023-03-13 00:00:19 -0700

    go

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for count in range(0, len(my_list):
-            print(my_list[count])
+    for count in range(0, len(my_list)):
+        print("{}".format(my_list[count]))
 
