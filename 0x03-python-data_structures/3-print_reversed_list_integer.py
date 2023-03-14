763d823a16255dcbf0238a3929d5d798c91f4661
zfiromsa <mdzm941@gmail.com>
2023-03-13 23:43:49 -0700

    go

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for element in my_list:
-        print("{:d}".format(element))
+    counter = len(my_list) - 1
+    while counter >= 0:
+        print("{:d}".format(my_list[counter]))
+        counter -= 1
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string1 = my_string.replase('c', '')
-    my_string2 = my_string.replase('C', '')
-    return my_string2
+    my_string1 = my_string.translate( { ord("c"): None } )
+    my_new_string = my_string1.translate( { ord("C"): None } )
+    return my_new_string
