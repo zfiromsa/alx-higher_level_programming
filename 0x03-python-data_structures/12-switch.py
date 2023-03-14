8036586537aecce545a29ef3ddab0dfdaac1d6b4
zfiromsa <mdzm941@gmail.com>
2023-03-13 17:42:25 -0700

    go

---
+++ 0x03-python-data_structures/12-switch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+a = 89
+b = 10
+tmp = b
+b = a
+a = tmp
+print("a={:d} - b={:d}".format(a, b))
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    my_string1 = my_string.replase('c', '')
+    my_string2 = my_string.replase('C', '')
+    return my_string2
