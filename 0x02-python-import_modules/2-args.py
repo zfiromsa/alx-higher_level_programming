bd3b9bafd82d51d92b7d7e2ebf437462a284b20a
zfiromsa <mdzm841@gmail.com>
2023-11-30 17:38:39 -0800

    ch index 2, 3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments.".format(len(sys.argv) - 1))
         for i in range(1, len(sys.argv)):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+            print("{}: {}".format(i + 1, sys.argv[i]))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 import sys
 if __name__ == "__main__":
     sum = 0
-    for i in range(1, len(sys.argv) + 1):
+    for i in range(1, len(sys.argv)):
         sum += int(sys.argv[i])
     print("{}".format(sum))
