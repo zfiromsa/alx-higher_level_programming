f37a76bfade7852625ecf9677ffe5b79b759bfbe
zfiromsa <mdzm841@gmail.com>
2023-11-30 21:37:17 -0800

    go

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 2:
         print("{} argument.".format(len(sys.argv) - 1))
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
