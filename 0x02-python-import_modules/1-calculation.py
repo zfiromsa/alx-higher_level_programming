a675928c1606e618e11356228d6df07e642488ba
zfiromsa <mdzm841@gmail.com>
2023-11-30 17:11:01 -0800

    goh

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-add_result = add(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, add_result))
-sub_result = sub(a, b)
-print("{:d} - {:d} = {:d}".format(a, b, sub_result))
-mul_result = div(a, b)
-print("{:d} * {:d} = {:d}".format(a, b, mul_result))
-div_result = div(a, b)
-print("{:d} / {:d} = {:d}".format(a, b, div_result))
+
+if __name__ == "__main__":
+    a = 10
+    b = 5
+
+    add_result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, add_result))
+    sub_result = sub(a, b)
+    print("{:d} - {:d} = {:d}".format(a, b, sub_result))
+    mul_result = div(a, b)
+    print("{:d} * {:d} = {:d}".format(a, b, mul_result))
+    div_result = div(a, b)
+    print("{:d} / {:d} = {:d}".format(a, b, div_result))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys
-agc = len(sys.argv)
-print("{:d} arguments.".format(agc))
-for i in range(0, agc):
-    print("{:d}: {:s}".format(i + 1, sys.argv[i]))
+if __name__ == "__main__":
+    agc = len(sys.argv)
+
+    print("{:d} arguments.".format(agc))
+    for i in range(0, agc):
+        print("{:d}: {:s}".format(i + 1, sys.argv[i]))
