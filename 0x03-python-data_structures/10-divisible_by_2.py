9e09953b7c1c7198821aa8ada398d959d3dbf2ff
zfiromsa <mdzm841@gmail.com>
2023-12-04 22:52:58 -0800

    go

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    ret_list = []
+    for i in my_list:
+        if (my_list[i] % 2) == 0:
+            ret_list.append(True)
+        else:
+            ret_list.append(False)
+    return ret_list
