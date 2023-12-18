a23c3bad2eb1621bb54f6dc8c4d2aa987c01832e
zfiromsa <mdzm841@gmail.com>
2023-12-18 01:42:53 -0800

    0x05-python-exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,8 @@ def safe_print_list(my_list=[], x=0):
             count += count;
     except IndexError:
         print("sorry that index doent exist")
+    except TypeError:
+        print("Type error")
     else:
         print(end="\n")
         return count
