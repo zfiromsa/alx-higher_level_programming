2d73c97a5da2c32850795cc46b1b7e1840917ab9
zfiromsa <mdzm841@gmail.com>
2023-12-18 02:38:17 -0800

    go

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    
+    try:
+        print("{:d}".format(value))
+    except TypeError:
+        print("TypeError\n")
+    else:
+        return True
+    finally:
+        return False
