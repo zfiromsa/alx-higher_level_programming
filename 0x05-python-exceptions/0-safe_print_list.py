cd0da536585588cf3f408ec0c2392221ea134fd6
zfiromsa <mdzm841@gmail.com>
2023-12-18 02:49:15 -0800

    go

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
             count += 1
+    except IndexError:
+        pass
+    else:
         print(end="\n")
         return count
-    except IndexError:
-        pass 
