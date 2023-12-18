08806b4522f7792d59a519c9a4f97e8624cafbbb
zfiromsa <mdzm841@gmail.com>
2023-12-18 01:38:24 -0800

    0x05-python-exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in x:
+            print("{:d}".format(my_list[i]), end="\n")
+            count += count;
+    except IndexError:
+        print("sorry that index doent exist")
+    else:
+        print(end="\n")
+        return count
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,4 @@
+#README
+0x05. Python - Exceptions
+Python
+
