e9e471f58f48c26e7965e16db23cc126d4511537
zfiromsa <mdzm841@gmail.com>
2023-12-18 02:33:06 -0800

    go

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,10 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print("{:d}".format(my_list[i]), end="")
+            print(my_list[i], end="")
             count += 1
     except IndexError:
         print("sorry that index doent exist")
-    except TypeError:
-        print("Type error")
     else:
         print(end="\n")
         return count
