5a037433f81d3fc570ada6b5ccbfe209d0e65d79
zfiromsa <mdzm841@gmail.com>
2023-12-18 10:48:25 -0800

    go

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
-    result = None
-    val = "Exception: Unknown format code 'd' for object of type 'str'"
     try:
         print("{:d}".format(value), end="\n")
-        result = True
-    except TypeError:
-        raise Exception(val)
-        print(value, "is not an integer")
-        result = False
-    return result 
+        return True
+    except Exception as e:
+        import sys
+        print("Exception: {}".format(e), file=sys.stderr)
+        return False
