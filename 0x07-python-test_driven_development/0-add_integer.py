4872ae79e772fdf9d0652170a7867f2ba4d0cda6
zfiromsa <mdzm841@gmail.com>
2024-01-03 02:37:22 -0800

    go

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,4 +4,4 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not type(a, (int, float)):
         raise TypeError("b must be an integer")
-    return a + b
+    return int(a) + int(b)
