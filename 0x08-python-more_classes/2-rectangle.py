dda6e443e863f68f50aefc9447e5cf7267bdca75
zfiromsa <mdzm841@gmail.com>
2024-01-05 01:28:21 -0800

    go

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,7 +3,7 @@
 This is a module for defining a Rectangle class.
 """
 
-class Rectangle:
+class  Rectangle:
     """
     This is a Rectangle CLASS.
     It current doesn't have any attributes or methodes.
