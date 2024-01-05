3d46dfb3c1eeddb5ccc7b0f3c7406b02273651c9
zfiromsa <mdzm841@gmail.com>
2024-01-04 17:11:44 -0800

    comment

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 class Rectangle:
-    """ Rectangle CLASS """
+    """A Class Rectangle defines a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """
+        Initialize a new Rectangle.
+
+        :param width: the width of rectangle. Default to 0.
+        :param height: the height of rectangle. Default to 0.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of Rectangle."""
         return self.width
     
     @width.setter
     def width(self, value):
+        """
+        Set the width of Rectangle
+
+        :param value: The new width of the reactangle.
+        :raises TypeError: If the width is not an integer
+        :raises ValueError: If the width is less than 0
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +34,18 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of Rectangle."""
         return self.height
     
     @height.setter
     def height(self, value):
+        """
+        Set the height of Rectangle
+
+        :param value: The new height of the reactangle.
+        :raises TypeError: If the height is not an integer
+        :raises ValueError: If the height is less than 0
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
 class Rectangle:
-    """ Rectangle CLASS """
+    """A Class Rectangle defines a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """
+        Initialize a new Rectangle.
+
+        :param width: the width of rectangle. Default to 0.
+        :param height: the height of rectangle. Default to 0.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of Rectangle."""
         return self.width
     
     @width.setter
     def width(self, value):
+        """
+        Set the width of Rectangle
+
+        :param value: The new width of the reactangle.
+        :raises TypeError: If the width is not an integer
+        :raises ValueError: If the width is less than 0
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +34,18 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of Rectangle."""
         return self.height
     
     @height.setter
     def height(self, value):
+        """
+        Set the height of Rectangle
+
+        :param value: The new height of the reactangle.
+        :raises TypeError: If the height is not an integer
+        :raises ValueError: If the height is less than 0
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -30,9 +53,15 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Calculate the area of Rectangle and Return area of rectangle"""
         return self.__height * self.__width
     
     def perimeter(self):
+        """
+        Calculate the perimeter of Rectangle and Return perimeter of rectangle
+
+        :return: The perimeter of rectangle or 0 if either the width or heighr is 0.
+        """
         if self.__width == 0 or self.height == 0:
             return 0
         return 2 * (self.__width + self.__height)
