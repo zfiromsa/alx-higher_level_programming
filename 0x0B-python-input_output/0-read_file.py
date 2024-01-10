7ddf173277a676105e621e8456b6ddded65c0b22
zfiromsa <mdzm841@gmail.com>
2024-01-09 22:35:43 -0800

    g

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a function that reads a text file (UTF8) and prints it to stdout:"""
 
+
 def read_file(filename=""):
     """
     This function read a test file (utf8) and print it to stdout.
