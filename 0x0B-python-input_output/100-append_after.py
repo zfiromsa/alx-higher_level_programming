2a5f08de8f810c06c06284fdb4e4892e15e2a6c6
zfiromsa <mdzm841@gmail.com>
2024-01-10 05:54:24 -0800

    go

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""""Import json modul"""
+import json
+
+def append_after(filename="", search_string="", new_string=""):
+    """"
+    Insert a line of text a file  after each line containing a specific string.
+
+    Args:
+        filename (str): the name of the file.
+        search_string (str): The specific string to search for in each line.
+        new_string (str): The line of text to inser after  each line containing
+        the search string.  
+    """"
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines =f.readlines()
+
+    with open(filename, 'W', encoding="utf-8") as file:
+        for line in lines:
+            f.write(line)
+            if search_string in line:
+                f.write(new_string + '\n')
