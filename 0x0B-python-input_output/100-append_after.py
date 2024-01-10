138f010e60595b8724f555a93a3eb65b274fb4cb
zfiromsa <mdzm841@gmail.com>
2024-01-10 05:56:11 -0800

    go

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 """"Import json modul"""
 import json
+"""
+THIS function that inserts a line of text to a file, after each
+line containing a specific string (see example):
+"""
+
 
 def append_after(filename="", search_string="", new_string=""):
     """"
