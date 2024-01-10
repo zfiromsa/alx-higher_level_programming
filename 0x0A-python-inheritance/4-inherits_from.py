8ed81e56b81cdbc497550acbeb7af97d57510de0
zfiromsa <mdzm841@gmail.com>
2024-01-10 15:11:50 -0800

    go

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+"""
+a function that returns True if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class ; otherwise False
+"""
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    Return true or false depend object is an instance
 
-
-    
+    Args:
+        obj: object.
+        a_class: class.
+    Returns:
+        boolean value true or false.
+    """
     if isinstance(obj, a_class):
         return True
     else:
