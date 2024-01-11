818005fa35e48c7453db8c251a98128bbfd46fa4
zfiromsa <mdzm841@gmail.com>
2024-01-10 23:26:44 -0800

    go

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -2,5 +2,10 @@
 """
 empty class Square that defines a square:
 """
+
+
 class Square:
+    """
+    empty class Square that defines a square.
+    """
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """
-A class Square that defines a square by: (based on 0-square.py)
-
-Private instance attribute: size
-Instantiation with size (no type/value verification)
+A class Square that defines a square
 """
+
 class Square:
+    """
+    A class that defines a square by its size.
+
+    Attrirbutes:
+    size (int): size of a the square
+
+    Methods:
+    None for now.
+    """
     def __init__(self, size):
+        """
+        Constructs a new instance of the squere class.
+
+        parameters:
+        size (int): the size of a side of the square
+        """
         self.__size = size
