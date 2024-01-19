283c22e58c2e1207e04e3b5c1097f632f25a594e
zfiromsa <mdzm841@gmail.com>
2024-01-18 18:31:37 -0800

    go

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -148,7 +148,7 @@ class Rectangle(Base):
         return self.__height * self.__width
     
     def __str__(self):
-        return f"[Rectangle] ({self.id} {self.x}/{self.y} - {self.width}/{self.height})"
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
     def display(self):
         """
         This models prints in stdout the Rectangle instance 
