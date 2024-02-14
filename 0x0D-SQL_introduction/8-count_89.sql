fa8ce748ae97d01f5b0a23efc864b13b19c62775
zfiromsa <mdzm841@gmail.com>
2024-02-14 06:34:23 -0800

    so

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- A script that displays the number of records with id = 89 in the 
 -- table first_table of the database hbtn_0c_0 in
-SELECT COUNT(*) FROM first_table WHERE i = 89;
+SELECT COUNT(id) FROM first_table WHERE i = 89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- A script that creates a table second_table in the database
 -- hbtn_0c_0 in your MySQL server and add multiples rows.
-CREAT TABLE IF NOT EXISTS $D=database.second_table (
+CREAT TABLE IF NOT EXISTS second_table (
     id INT PRIMARY KEY,
     name VARCHAR(256),
     score INT
 );
 
-INSERT INTO $database.second_table (id, name, score) VALUE(1, 'John', 10);
-INSERT INTO $database.second_table (id, name, score) VALUE(2, 'Alex', 3);
-INSERT INTO $database.second_table (id, name, score) VALUE(3, 'Bob', 14);
-INSERT INTO $database.second_table (id, name, score) VALUE(4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUE(1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUE(2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUE(3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUE(4, 'George', 8);
