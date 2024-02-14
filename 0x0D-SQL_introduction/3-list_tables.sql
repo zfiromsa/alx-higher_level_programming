1fe5038e730a6b343e921ef71833bdf539d55b9a
zfiromsa <mdzm841@gmail.com>
2024-02-14 05:06:01 -0800

    go

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,9 +1,2 @@
-#!/usr/bin/bash
 -- A script that lists all the tables of a database in your MySQL server.
-
-if [ $# -ne 1]; then
-    echo "Usage: $0 <database_name>"
-    exit 1
-fi
-database="$1"
-mysql -u root -p -e "USER $database; SHOW TABLE;"
+SHOW TABLES;
