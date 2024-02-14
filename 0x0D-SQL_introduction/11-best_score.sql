b90cc12fe2530fb9f5002d5f689cff931633c2a9
zfiromsa <mdzm841@gmail.com>
2024-02-14 06:23:31 -0800

    so

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
---- A script that creates a table second_table in the database hbtn_0c_0 in
+-- A script that creates a table second_table in the database hbtn_0c_0 in
 -- your MySQL server and add multiples rows.
-SELECT score, name FROM second_table WHERE score >= 10 BY score DESC
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
---- A script that prints the full description of the table first_table from 
---- the database hbtn_0c_0 in your MySQL serve
+-- A script that prints the full description of the table first_table from 
+-- the database hbtn_0c_0 in your MySQL serve
 SHOW CREATE TABLE first_table;
