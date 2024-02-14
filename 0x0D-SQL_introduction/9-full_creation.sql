b399477fcca973174c17537aa604ad081eb258b0
zfiromsa <mdzm841@gmail.com>
2024-02-14 06:43:06 -0800

    so

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,6 @@ CREAT TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-
 INSERT INTO second_table (id, name, score) VALUE(1, 'John', 10);
 INSERT INTO second_table (id, name, score) VALUE(2, 'Alex', 3);
 INSERT INTO second_table (id, name, score) VALUE(3, 'Bob', 14);
