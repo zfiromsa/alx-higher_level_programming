55963de0ccbadab447058ecfa5b6499eca4621f8
zfiromsa <mdzm841@gmail.com>
2024-02-14 23:49:25 -0800

    some correction

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -8,6 +8,6 @@
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.states_id = states.id
+WHERE cities.state_id = states.id
 AND states.name = 'California'
 ORDER BY cities.id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -6,5 +6,5 @@
 USE hbtn_0d_usa;
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities, states
-WHERE cities.state_id = state.id
+WHERE cities.state_id = states.id
 ORDER BY cities.id ASC;
