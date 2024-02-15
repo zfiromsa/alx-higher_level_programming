cac9f4c7753561a6e0a55e8b0c7b77cf22b5312c
zfiromsa <mdzm841@gmail.com>
2024-02-15 01:04:02 -0800

    so

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,6 @@
 -- A script that displays the number of records with id = 89 in the 
 -- table first_table of the database hbtn_0c_0 in
-SELECT COUNT(id) FROM first_table WHERE i = 89;
+USE hbtn_0c_0;
+SELECT COUNT(*) AS RE_COU
+FROM first_table
+WHERE i = 89;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,9 +4,4 @@
 --     Results must be sorted in descending order by their rating
 --     You can use only one SELECT statement
 --     The database name will be passed as an argument of the mysql command
-SELECT tv_genres.name, SUM(tv_show.rate) AS rating_sum
-FROM tv_genres
-JOIN tv_genres ON tv_genres.id = tv_show_genres.genres_id
-JOIN tv_show ON tv_show_genres.show_id = tv_show.id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+
