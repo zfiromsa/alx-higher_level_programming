c4699ab1dfaa9fb15a4510d362ef345e1e86d569
zfiromsa <mdzm841@gmail.com>
2024-03-12 01:43:57 -0700

    go

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const [,,firts_arg, second_arg] = args;
+console.log("${firts_arg} is ${second_arg}");
+
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const args = process.argv;
+const first_no = Number.args[2];
+console.log(Nomber.isNaN(first_no) ? "Not a numbe" : "My number: ${first_no}");
+
