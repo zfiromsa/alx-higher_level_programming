8962a2a473494a348c282b526132359365797063
zfiromsa <mdzm841@gmail.com>
2024-03-12 02:10:24 -0700

    go

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
 
 const args = process.argv;
-const first_no = Number.args[2];
-console.log(Number.isNaN(first_no) ? "Not a numbe" : "My number: ${first_no}");
+const first_no = Number(args[2]);
+if (Number.isNaN(first_no))
+{
+    console.log("Not a numbe");
+}
+else
+{
+    console.log("My number: ${first_no}");
+}
 
