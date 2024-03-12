284518e64532eca3bed848ecc9fd247849719ddd
zfiromsa <mdzm841@gmail.com>
2024-03-12 00:53:02 -0700

    go

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+const args = process.arg.slice(2);
+
+if (args.length === 0)
+{
+    console.log("No argument");
+}
+else if (args.length === 1)
+{
+    console.log("Argument found");
+}
+else (args.length === 0)
+{
+    console.log("Arguments found");
+}
+
