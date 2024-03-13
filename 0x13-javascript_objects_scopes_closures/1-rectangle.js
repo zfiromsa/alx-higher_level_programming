c128d4f83b3b2bfc20ec99cda00167e941efbdcb
zfiromsa <mdzm841@gmail.com>
2024-03-13 02:14:09 -0700

    go

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constuctor(w, h) {
-        this.width = w;
-        this.height = h;
-    }
+	constuctor(w, h) {
+		this.width = w;
+		this.height = h;
+	}
 }
 module.exports = Rectangle;
+
