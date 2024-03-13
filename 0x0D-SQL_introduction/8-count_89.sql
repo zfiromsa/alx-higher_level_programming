00820b3911e6d7c0d18b4b76995f0e74768faaa5
zfiromsa <mdzm841@gmail.com>
2024-03-13 01:32:46 -0700

    go

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,6 +1,5 @@
 -- A script that displays the number of records with id = 89 in the 
 -- table first_table of the database hbtn_0c_0 in
-USE hbtn_0c_0;
-SELECT COUNT(*) AS RE_COU
+SELECT COUNT('id') AS RE_COU
 FROM first_table
-WHERE i = 89;
+WHERE id = 89;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,9 @@ CREAT TABLE IF NOT EXISTS second_table (
     name VARCHAR(256),
     score INT
 );
-INSERT INTO second_table (id, name, score) VALUE(1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUE(2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUE(3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUE(4, 'George', 8);
+INSERT INTO hbtn_0c_0.second_table (id, name, score)
+VALUE
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
