a9cadb43a18726fe15bab14e4a3c24fc7243b5c5
zfiromsa <mdzm841@gmail.com>
2024-03-22 18:22:18 -0700

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,7 +14,8 @@ if __name__ == "__main__":
         cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
         rows = cur.fetchall()
         for row in rows:
-            print(row)
+            if row[1][0] == 'N':
+                print(row)
     except MySQLdb.Error as err:
         print("ERROR: ", err)
 
